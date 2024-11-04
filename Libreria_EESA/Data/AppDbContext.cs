738e6874fb9c11b4b41970e6732f8ca49e124d62
beto200519 <norbertocarrillo2005@gmail.com>
2024-11-04 08:46:25 -0700

    Agregar archivos de proyecto.

---
+++ Libreria_EESA/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using libreria_EESA.Data.Models;
+using Microsoft.EntityFrameworkCore;
+namespace libreria_EESA.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
