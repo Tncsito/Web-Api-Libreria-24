f72c46b758026153b1f3c6f6192ceefa247fc2c2
beto200519 <norbertocarrillo2005@gmail.com>
2024-11-05 08:58:05 -0700

    3.4

--- Libreria_EESA/Controllers/BooksController.cs
+++ Libreria_EESA/Controllers/BooksController.cs
@@ -13,10 +13,33 @@ namespace Libreria_EESA.Controllers
         {
             _booksService = booksService;
         }
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBooks()
+        {
+            var allbooks = _booksService.GetAllBks();
+            return Ok(allbooks);
+        }
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _booksService.GetBookById(id);
+            return Ok(book);
+        }
         [HttpPost("add-book")]
         public IActionResult Addbook([FromBody]BookVM book)
         {
             _booksService.AddBook(book);
+            return Ok();
+        }
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody]BookVM book)
+        {
+            var updateBook = _booksService.UpdateBookByID(id, book);
+            return Ok(updateBook);
+        }
+        public IActionResult DeleteBookId(int id)
+        {
+            _booksService.DeleteBookById(id);
             return Ok();
         }
     }
--- Libreria_EESA/Data/Services/BooksService.cs
+++ Libreria_EESA/Data/Services/BooksService.cs
@@ -2,6 +2,9 @@
 using libreria_EESA.Data.Models;
 using Libreria_EESA.Data.ViewModels;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace Libreria_EESA.Data.Services
 {
@@ -28,6 +31,35 @@ namespace Libreria_EESA.Data.Services
             };
             _context.Books.Add(_book);
             _context.SaveChanges();
+        }
+        public List<Book> GetAllBks() => _context.Books.ToList(); //GET por cosas
+        public Book GetBookById(int bookid) => _context.Books.FirstOrDefault(n => n.id == bookid); //GET por id
+        public Book UpdateBookByID(int bookid, BookVM book)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.id == bookid);
+            if(_book != null)
+            {
+                _book.Titulo = book.Titulo;
+                _book.Descripcion = book.Descripcion;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.DateRead;
+                _book.Rate = book.Rate;
+                _book.Genero = book.Genero;
+                _book.Autor = book.Autor;
+                _book.CoverURL = book.CoverURL;
+
+                _context.SaveChanges();
+            }
+            return _book;
+        }
+        public void DeleteBookById(int bookid)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.id == bookid);
+            if (_book != null)
+            {
+                _context.Books.Remove( _book );
+                _context.SaveChanges();
+            }
         }
     }
 }
